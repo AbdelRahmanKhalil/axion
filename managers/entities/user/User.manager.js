ec96592610121d60668ac1b744898d6d6fda4da7
akhalil <akhalil@192.168.206.1>
2024-02-12 19:25:56 +0200

    Handled user insertion in DB

--- managers/entities/user/User.manager.js
+++ managers/entities/user/User.manager.js
@@ -8,11 +8,40 @@ module.exports = class User {
         this.tokenManager        = managers.token;
         this.usersCollection     = "users";
         this.userExposed         = ['createUser'];
+        this.httpExposed         = ['createUser'];
+        this.cache               = cache;
     }
 
-    async createUser({username, email, password}){
-        const user = {username, email, password};
+    insertUserInDb(user){
+        // Insert created user in DB (will not insert)
+        const userKey = `user:${user.username}`;
+        this.cache.hash.set({
+            key: userKey,
+            //data: userData
+            data: user
+        }).then(result => {
+            if(result > 0){
+                console.log('User data stored successfully:', result);
+            }
+            else if(result == 0){
+                console.log('User already exists:', result);
+            }
+            return result;
+            
+        }).catch(error => {
+            console.error('Error storing user data:', error);
+            return -1;
+        });
 
+        
+   }
+
+    
+
+    async createUser({username, email, password}){
+        console.log('-------------------UserManager createUser-------------------');
+        const user = {username, email, password};
+        console.log('username:'+user.username+' email:'+ user.email+' password:'+ user.password);
         // Data validation
         let result = await this.validators.user.createUser(user);
         if(result) return result;
@@ -21,11 +50,25 @@ module.exports = class User {
         let createdUser     = {username, email, password}
         let longToken       = this.tokenManager.genLongToken({userId: createdUser._id, userKey: createdUser.key });
         
+        // Insert user in database
+        let res = this.insertUserInDb(createdUser);
+        console.log('res='+res);
+
+        // Retrieve user data from DB
+        const userKey = `user:${username}`;
+        this.cache.hash.get({ key: userKey }).then(userData => {
+            console.log('User data retrieved successfully:', userData);
+        }).catch(error => {
+            console.error('Error retrieving user data:', error);
+        });
+
         // Response
         return {
             user: createdUser, 
             longToken 
         };
     }
+
+    
 
 }
